55012facb6bcaaa260b979a52eb05b0ca5479e38
Rafal Kapuscik <rkapuscik@antmicro.com>
2019-11-20 17:59:56 +0100

    Check if device implements tring descriptors
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- tests/test-enum.py
+++ tests/test-enum.py
@@ -36,18 +36,28 @@ def test_enumeration(dut):
         length=total_config_len,
         response=model.configDescriptor[1].get()[:total_config_len])
 
-    yield harness.get_string_descriptor(
-        lang_id=Descriptor.LangId.UNSPECIFIED,
-        idx=0,
-        response=model.stringDescriptor[0].get())
+    # Does the device report any string descriptors?
+    str_to_check = []
+    for idx in (
+                model.deviceDescriptor.iManufacturer,
+                model.deviceDescriptor.iProduct,
+                model.deviceDescriptor.iSerialNumber):
+        if idx != 0:
+            str_to_check.append(idx)
 
-    if model.stringDescriptor[0].wLangId:
-        # If the device implements string descriptors, let's try reading them
-        lang_id = model.stringDescriptor[0].wLangId[0]
+    # If the device implements string descriptors, let's try reading them
+    if str_to_check != []:
         yield harness.get_string_descriptor(
-            lang_id=lang_id,
-            idx=1,
-            response=model.stringDescriptor[lang_id][1].get())
+          lang_id=Descriptor.LangId.UNSPECIFIED,
+          idx=0,
+          response=model.stringDescriptor[0].get())
+
+        lang_id = model.stringDescriptor[0].wLangId[0]
+        for idx in str_to_check:
+            yield harness.get_string_descriptor(
+                lang_id=lang_id,
+                idx=idx,
+                response=model.stringDescriptor[lang_id][1].get())
 
     yield harness.set_configuration(1)
     # Device should now be in "Configured" state
